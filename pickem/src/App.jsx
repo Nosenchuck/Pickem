5f955fd24455e3ebb6d17d193988dd9194a0b952
Nosenchuck <pantingpug@yahoo.com>
2024-07-11 16:31:22 -0400

    1st

---
+++ pickem/src/App.jsx
@@ -0,0 +1,48 @@
+import { useState, useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  function fetchJSONData() {
+    fetch("https://www.thesportsdb.com/api/v1/json/3/search_all_teams.php?l=NFL")
+        .then((res) => {
+            if (!res.ok) {
+                throw new Error
+                    (`HTTP error! Status: ${res.status}`);
+            }
+            return res.json();
+        })
+        .then((data) => 
+              console.log(data))
+        .catch((error) => 
+               console.error("Unable to fetch data:", error));
+}
+fetchJSONData();
+
+  return (
+    <>
+      <div className='whole-page'>
+        <h1>Sports</h1>
+        
+        
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
